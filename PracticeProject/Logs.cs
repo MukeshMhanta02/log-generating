0ebefc2cc0dd432f68065b26af37e09a07828613
MukeshMhanta <MukeshMhanta@WSIN164>
2023-10-31 10:57:44 +0530

    Add project files.

---
+++ PracticeProject/Logs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Serilog;
+
+namespace PracticeProject
+{
+    class Logs
+    {
+       public static ILogger getLogFile()
+        {
+            string logFilePath = Path.Combine(@"E:\", "Errors","Errors.txt");
+            Console.WriteLine(logFilePath);
+           return Log.Logger = new LoggerConfiguration()
+          .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day) // Log to a file
+          .CreateLogger();
+        }
+    }
+}
---
+++ PracticeProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace PracticeProject
+{
+    enum weekDays
+    {
+        Monday = 0,
+        Teusday = 12,
+        Webdnesday = 14,
+        Thursday = 54,
+        Friday = 87,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(Path.Combine("C","MyDocuments"));
+
+
+            
+            try
+            {
+                int a = 0, b = 10, d;
+                d = b / a;
+
+            }
+            catch (Exception ex)
+            {
+                ILogger logs = Logs.getLogFile(); 
+                logs.Error(ex.Message, "Message");// Log the exception with a message
+            }
+            finally
+            {
+                Log.CloseAndFlush(); // Flush and close the log file
+            }
+            // Console.ReadKey();
+        }
+    }
+}
